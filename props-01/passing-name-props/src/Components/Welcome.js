83b4ddecaaeb5540be0df10188ec285cb7e3d14c
Aisha Abdullahi <boyaaisha@gmail.com>
2023-05-09 00:11:22 +0300

    p

--- props-01/passing-name-props/src/Components/Welcome.js
+++ props-01/passing-name-props/src/Components/Welcome.js
@@ -4,7 +4,7 @@ export default class welcome extends Component {
   render() {
     return (
       <div>
-        <h1>welcome {this.props.name}</h1>
+        <p>welcome {this.props.name}</p>
       </div>
     )
   }
