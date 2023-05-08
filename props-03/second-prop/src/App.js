aa5ccb9b9b48328d791a36f41540fd2c3c8a960b
Aisha Abdullahi <boyaaisha@gmail.com>
2023-05-09 00:55:05 +0300

    third props exercise done

---
+++ props-03/second-prop/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Welcome from './Components/Welcome';
+
+function App() {
+  return (
+    <div className="App">
+     <Welcome name = "Aisha" age ={12} />
+    </div>
+  );
+}
+
+export default App;
---
+++ props-03/second-prop/src/Components/Welcome.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Welcome extends Component {
+  render() {
+    return (
+        <>
+        <p>Welcome {this.props.name}</p>
+        <p>Your age is: {this.props.age}</p>
+        </>
+    )
+  }
+}
