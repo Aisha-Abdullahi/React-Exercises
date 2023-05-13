fc0936630fa5d2f5f3a19b64228c6b182d5b9ad9
Aisha Abdullahi <boyaaisha@gmail.com>
2023-05-13 10:54:21 +0300

    third state exercise done

---
+++ state-03/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Counter from './Components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+       <Counter initialCount= {0} interval ={1000} incrementValue={1}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ state-03/src/Components/Counter.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import CounterDisplay from "./CounterDisplay";
+
+export default class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: this.props.initialCount,
+    };
+
+    setInterval(() => {
+      this.setState({ count: this.state.count + this.props.incrementValue });
+    }, this.props.interval);
+  }
+
+  render() {
+    return (
+      <div>
+        <CounterDisplay count= {this.state.count}/>
+      </div>
+    );
+  }
+}
