c5c05e9cd8638fba7a9e22eae35dc212ed83e9ae
Aisha Abdullahi <boyaaisha@gmail.com>
2023-05-15 11:57:08 +0300

    Add todo

---
+++ lists-02/src/Todo.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+export default function Todo() {
+
+    const [todoItem, setTodoItem] =useState('')
+    const [items, setItems] = useState ([])
+
+    const handleTodo = (event) =>{
+        setTodoItem(event.target.value);
+    }
+
+    const handleAddItem = () =>{
+        if (todoItem !== '') {
+            setItems([...items, todoItem])
+
+            setTodoItem('')
+        }
+    }
+  return (
+    <div>
+    <input type='text' value={todoItem} onChange={handleTodo}/>
+    <button onClick={handleAddItem}>Add Task</button>
+    <ul>
+    {
+        items.map((item, index) => (
+            <li key={index}>{item}</li>
+        ))
+    }
+    </ul>
+    </div>
+  )
+}
